80d0fe2a9652cfe5395e21deb62ccc868548cb97
hoanganh209203 <129678025+hoanganh209203@users.noreply.github.com>
2024-04-21 21:36:14 +0700

    update

--- src/services/Music.ts
+++ src/services/Music.ts
@@ -4,7 +4,7 @@ export async function getMusic() {
       const response = await fetch(`https://spotify-be.vercel.app/track`);
       const data =await response.json();
       return data
-      // return response.json();
+
     } catch (error) {
       console.error('Lỗi khi gọi API:', error);
     }
@@ -15,7 +15,7 @@ export async function getMusic() {
       const data =await response.json();
       console.log(data);
       return data
-      // return response.json();
+
     } catch (error) {
       console.error('Lỗi khi gọi API:', error);
     }
